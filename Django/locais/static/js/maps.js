aa151a52e3f9c82980f328c1a7d84cd000f885ad
Adriano Martins <ampontes93@gmail.com>
2016-11-24 10:20:38 -0300

    botão de coordenadas para estabelecimentos feito na index

--- Django/locais/static/js/maps.js
+++ Django/locais/static/js/maps.js
@@ -9,3 +9,15 @@ function initMap() {
           map: map
         });
       }
+
+function upMap(latitude,longitude) {
+        var uluru = {lat:parseFloat(latitude), lng:parseFloat(longitude)};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
